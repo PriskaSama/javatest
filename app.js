f15667d7733138de241a320c8a9c6120db8a63fa
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2022-08-26 16:46:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import "./assets/css/styles.css";
+
+
+let button = document.getElementById("dark-mode");
+// pour vérifier si la valeur button est bien liée
+     console.log(button);  
+// Vérifier dans le navigateur
+
+let body = document.querySelector("body");
+
+function darkMode() {
+  body.classList.toggle("bg-dark");
+  body.classList.toggle("text-white");
+}
+
+button.addEventListener("click", darkMode);
